2f983fa86426011d0a021aaa5debc853a6229322
xlArtemlx <59087414+xlArtemlx@users.noreply.github.com>
2020-05-16 09:00:06 +0300

    Fixed bugs with field overflow and controller

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let main = document.querySelector('.main');
+const main = document.querySelector('.main');
+const scroeElem = document.getElementById("score");
+const levelElem = document.getElementById("level");
 let playfield = [
     [0,0,0,0,1,0,0,0,0,0],
     [0,0,0,0,1,0,0,0,0,0],
@@ -22,16 +24,85 @@ let playfield = [
     [0,0,0,0,2,2,0,0,0,0]
 ];
 
-let gameSpeed = 200;
+let score = 0;
+let currentLevel = 1;
+let possibleLevels = {
+  1: {
+    scorePerLine: 10,
+    speed: 400,
+    nextLevelScore: 20,
+  },
+  2: {
+    scorePerLine: 15,
+    speed: 300,
+    nextLevelScore: 500,
+  },
+  3: {
+    scorePerLine: 20,
+    speed: 200,
+    nextLevelScore: 1000,
+  },
+  4: {
+    scorePerLine: 30,
+    speed: 100,
+    nextLevelScore: 2000,
+  },
+  5: {
+    scorePerLine: 50,
+    speed: 50,
+    nextLevelScore: Infinity,
+  },
+};
+
 let activeTetro = {
-  x:0,
-  y:0,
-  shape:[
-    [1,1,1],
-    [0,1,0],
-    [0,1,0]
-  ]
-}
+  x: 0,
+  y: 0,
+  shape: [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+};
+
+let figures = {
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+  I: [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  L: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  J: [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  T: [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
+};
+
 
 function draw(){
     let mainInnerHTML = "";
@@ -72,51 +143,19 @@ function addActiveTetro() {
   }
 }
 
-function canTetroMoveDown() {
-    for (let i = 0; i < playfield.length; i++) {
-      for (let k = 0; k < playfield[i].length; k++) {
-        if (playfield[i][k] === 1) {
-          if (i === playfield.length - 1 || playfield[i + 1][k] === 2) {
-            return false;
-          }
-        }
-      }
-    }
-  
-    return true;
-  }
-  
-  function moveTetroDown() {
-    if (canTetroMoveDown()) {
-      for (let i = playfield.length - 1; i >= 0; i--) {
-        for (let k = 0; k < playfield[i].length; k++) {
-          if (playfield[i][k] === 1) {
-            playfield[i + 1][k] = 1;
-            playfield[i][k] = 0;
-          }
-        }
-      }
-    } else {
-      fixTetro();
-    }
-  }
 
-function fixTetro(){
-    for (let i = 0; i < playfield.length; i++) {
-        for (let k = 0; k < playfield[i].length; k++){
-            if(playfield[i][k] === 1){
-                playfield[i][k] = 2;
-            }
-        }
-    }
-    playfield[0] = [0,0,0,0,1,1,0,0,0,0];
-    playfield[1] = [0,0,0,0,1,1,0,0,0,0];
+function rotateTetro() {
+  const prevTetroState = activeTetro.shape;
+
+  activeTetro.shape = activeTetro.shape[0].map((val, index) =>
+    activeTetro.shape.map((row) => row[index]).reverse()
+  );
+
+  if (hasCollisions()) {
+    activeTetro.shape = prevTetroState;
+  }
 }
-   
 
-
-
-// проверка на выход за поля
 function hasCollisions() {
   for (let i = 0; i < activeTetro.shape.length; i++) {
     for (let k = 0; k < activeTetro.shape[i].length; k++) {
@@ -133,51 +172,74 @@ function hasCollisions() {
   return false;
 }
 
-
 function removeFullLines() {
-    let canRemoveLine = true;
-    let summ = 0;
-    for (let i = 0; i < playfield.length; i++) {
-      for (let k = 0; k < playfield[i].length; k++) {
-        summ = summ += k
-        if (playfield[i] !== 2) {
-          canRemoveLine = false;
-<<<<<<< HEAD
-        } 
+  let canRemoveLine = true,
+    filledLines = 0;
+  for (let i = 0; i < playfield.length; i++) {
+    for (let k = 0; k < playfield[i].length; k++) {
+      if (playfield[i][k] !== 2) {
+        canRemoveLine = false;
+        break;
       }
     }
-    for (let i = 0; i < playfield.length; i++){
-      for (let k = 0; k < playfield[i].length; k++){
-        if(canRemoveLine){
-          playfield[i].splice(k, 1, 0);
-      }
+    if (canRemoveLine) {
+      playfield.splice(i, 1);
+      playfield.splice(0, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+      filledLines += 1;
     }
-  }
-}
-  removeFullLines()
-
-document.onkeydown = function(e){
-    if(e.keyCode === 37){
-        moveTetroLeft();
-    } else if(e.keyCode === 39){
-        moveTetroRight()
-    } else if(e.keyCode === 40){
-        // ускоряем фигурку
-    }
-=======
-          break;
-        }
-      }
-      if (canRemoveLine) {
-        playfield.splice(y, 1);
-     }
     canRemoveLine = true;
   }
->>>>>>> 26023328f86fb3bfe363f131bcd6d52f7c01f8fb
+
+  switch (filledLines) {
+    case 1:
+      score += 10;
+      break;
+    case 2:
+      score += 10 * 3;
+      break;
+    case 3:
+      score += 10 * 6;
+      break;
+    case 4:
+      score += 10 * 12;
+      break;
+  }
+
+  scroeElem.innerHTML = score;
+
+  if (score >= possibleLevels[currentLevel].nextLevelScore) {
+    currentLevel++;
+    levelElem.innerHTML = currentLevel;
+  }
 }
-addActiveTetro()
-draw()
-  
+
+function getNewTetro() {
+  const possibleFigures = "IOLJTSZ";
+  const rand = Math.floor(Math.random() * 7);
+  return figures[possibleFigures[rand]];
+}
+
+function fixTetro() {
+  for (let i = 0; i < playfield.length; i++) {
+    for (let k = 0; k < playfield[i].length; k++) {
+      if (playfield[i][k] === 1) {
+        playfield[i][k] = 2;
+      }
+    }
+  }
+}
+
+function moveTetroDown() {
+  activeTetro.y += 1;
+  if (hasCollisions()) {
+    activeTetro.y -= 1;
+    fixTetro();
+    removeFullLines();
+    activeTetro.shape = getNewTetro();
+    activeTetro.x = Math.floor((10 - activeTetro.shape[0].length) / 2);
+    activeTetro.y = 0;
+  }
+}
 
 document.onkeydown = function (e) {
   if (e.keyCode === 37) {
@@ -199,11 +261,19 @@ document.onkeydown = function (e) {
   addActiveTetro();
   draw();
 };
-removeFullLines()
-function startGame(){
-    moveTetroDown()
-    draw()
-    setTimeout(startGame, gameSpeed)
+
+scroeElem.innerHTML = score;
+levelElem.innerHTML = currentLevel;
+
+addActiveTetro();
+draw();
+
+function startGame() {
+  moveTetroDown();
+  addActiveTetro();
+  draw();
+  setTimeout(startGame, possibleLevels[currentLevel].speed);
 }
-setTimeout(startGame, gameSpeed)
+
+setTimeout(startGame, possibleLevels[currentLevel].speed);
 
